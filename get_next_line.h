fb2bb0a273a892ef391190ee453169d710248e97
Marius BANCIU <mabanciu@b1p27.academyplus.ro>
2018-03-15 13:47:51 +0200

    files

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabanciu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/25 13:50:52 by mabanciu          #+#    #+#             */
+/*   Updated: 2018/02/14 14:44:11 by mabanciu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 10000
+
+int		get_next_line(const int fd, char **line);
+
+#endif
---
+++ libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabanciu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/14 12:54:29 by mabanciu          #+#    #+#             */
+/*   Updated: 2018/02/14 15:13:25 by mabanciu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnew(size_t size);
+void	ft_bzero(void *s, size_t len);
+void	*ft_memalloc(size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
